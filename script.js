38e704204027550b493d9960e9416ef9b1c4ce15
the-moezzat <ezzat7014@gmail.com>
2022-11-14 18:51:43 +0200

    feat: toggle between different systems

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const arrivalTime = document.querySelector("#arrival");
 const serviceTime = document.querySelector("#service");
 const maxCustomer = document.querySelector("#max");
 const initCustomer = document.querySelector("#init");
+const maxInit = document.querySelector(".horiz");
+const mode = document.querySelector(".mode");
+
+let activeChoice = mode.querySelector(".choice--active");
 
 const cases = function () {
   const lambda = 1 / parseInt(arrivalTime.value);
@@ -11,9 +15,13 @@ const cases = function () {
   if (!(lambda && mu)) return;
 
   if (lambda > mu) {
+    maxCustomer.closest(".input").classList.remove("input-disable");
+    initCustomer.closest(".input").classList.add("input-disable");
     initCustomer.disabled = true;
     maxCustomer.disabled = false;
   } else {
+    maxCustomer.closest(".input").classList.add("input-disable");
+    initCustomer.closest(".input").classList.remove("input-disable");
     initCustomer.disabled = false;
     maxCustomer.disabled = true;
   }
@@ -28,4 +36,57 @@ serviceTime.addEventListener("keyup", (e) => {
   const mu = 1 / serviceTime.value;
 
   cases();
+});
+
+mode.addEventListener("click", (e) => {
+  const choice = e.target;
+  if (!choice.classList.contains("choice")) return;
+
+  const type = choice.dataset.type;
+
+  activeChoice.classList.remove("choice--active");
+  choice.classList.add("choice--active");
+  activeChoice = choice;
+
+  switch (type) {
+    case "dd1k":
+      console.log(type);
+      maxInit.classList.remove("invisible");
+      initCustomer.closest(".input").classList.remove("invisible");
+      maxCustomer.closest(".input").querySelector(".input__title").textContent =
+        "Maximum customer (K - 1)";
+      initCustomer
+        .closest(".input")
+        .querySelector(".input__title").textContent = "Initial Customer (M)";
+      break;
+    case "mm1":
+      console.log(type);
+      maxInit.classList.add("invisible");
+      break;
+    case "mm1k":
+      maxInit.classList.remove("invisible");
+      initCustomer.closest(".input").classList.add("invisible");
+      maxCustomer.closest(".input").querySelector(".input__title").textContent =
+        "Maximum customer (K)";
+      console.log(type);
+      break;
+    case "mmc":
+      maxInit.classList.remove("invisible");
+      initCustomer.closest(".input").classList.add("invisible");
+      maxCustomer.closest(".input").querySelector(".input__title").textContent =
+        "systems (C)";
+      console.log(type);
+      break;
+    case "mmck":
+      maxInit.classList.remove("invisible");
+      initCustomer.closest(".input").classList.remove("invisible");
+
+      maxCustomer.closest(".input").querySelector(".input__title").textContent =
+        "Maximum customer (K)";
+      initCustomer
+        .closest(".input")
+        .querySelector(".input__title").textContent = "systems (C)";
+      console.log(type);
+      break;
+  }
 });
